73f5a737dd1aafae1fdbe44adc722b83777038cc
HericMD <hericmdms@gmail.com>
2023-03-02 15:50:18 -0300

    Livraria-django

---
+++ livraria/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from livraria.models import Categoria, Editora, Autor, Livro
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+
+
+# Register your models here.
