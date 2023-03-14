b413fedb07c71fbdbb7f78c1e266e7b294694eae
HericMD <hericmdms@gmail.com>
2023-03-14 16:42:13 -0300

    passo-18

--- livraria/models/livro.py
+++ livraria/models/livro.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from livraria.models import Autor, Categoria, Editora
 
+from uploader.models import Image
 
 class Livro(models.Model):
 
@@ -16,5 +17,14 @@ class Livro(models.Model):
         Editora, on_delete=models.PROTECT, related_name="livros"
         )
     autores = models.ManyToManyField(Autor, related_name="livros")
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
+    )
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
+    
